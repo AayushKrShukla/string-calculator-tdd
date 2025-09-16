250a73cd0bda1516ac3d87b5634306c428d5a3ec
Aayush Shukla <56974422+AayushKrShukla@users.noreply.github.com>
2025-09-17 00:11:59 +0530

    RED: add testcase to verify negative numbers throw error

--- string-calculator.test.ts
+++ string-calculator.test.ts
@@ -21,18 +21,23 @@ describe("String Calculator", () => {
   });
 
   test("should return sum of any amount of comma separated values", () => {
-    expect(calculator.add("1,2,3,4,5")).toBe(15)
-    expect(calculator.add("27,14,40,11,18")).toBe(110)
-    expect(calculator.add("1, 2, 3")).toBe(6)
-  })
+    expect(calculator.add("1,2,3,4,5")).toBe(15);
+    expect(calculator.add("27,14,40,11,18")).toBe(110);
+    expect(calculator.add("1, 2, 3")).toBe(6);
+  });
 
   test("should handle new line between the numbers and return the sum", () => {
-    expect(calculator.add("1\n2,3,4\n5")).toBe(15)
-    expect(calculator.add("1,2\n3\n4,5")).toBe(15)
-  })
+    expect(calculator.add("1\n2,3,4\n5")).toBe(15);
+    expect(calculator.add("1,2\n3\n4,5")).toBe(15);
+  });
 
   test("should allow custom delimiter to separate values", () => {
     expect(calculator.add("//;\n1;2;3")).toBe(6);
     expect(calculator.add("//;\n7;11")).toBe(18);
+  });
+
+  test("expect an error if input contains negative numbers", () => {
+    expect(() => calculator.add("1,2,-3")).toThrow(new Error("negative numbers not allowed -3"))
+    expect(() => calculator.add("//;\n1;2;-3;-4")).toThrow(new Error("negative numbers not allowed -3,-4"))
   });
 });
