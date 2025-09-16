8d3af874bbdda41be9ad36f977ff466d9687141d
Aayush Shukla <56974422+AayushKrShukla@users.noreply.github.com>
2025-09-16 22:36:38 +0530

    RED: add testcase to handle any amount of numbers

--- string-calculator.test.ts
+++ string-calculator.test.ts
@@ -15,8 +15,12 @@ describe("String Calculator", () => {
     expect(calculator.add("27")).toBe(27);
   });
 
-  test("should return sum of comma separated value", () => {
+  test("should return sum of two comma separated value", () => {
     expect(calculator.add("5,6")).toBe(11);
     expect(calculator.add("9,64")).toBe(73);
   });
+
+  test("should return sum of any amount of comma separated values", () => {
+    expect(calculator.add("1,2,3,4,5")).toBe(15)
+  })
 });
